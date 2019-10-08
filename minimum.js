d575e3b144ddf12ef4ad53ffd9e5802977ff8acb
Alex Abushady <aabushady214@gmail.com>
2019-10-08 10:49:41 -0400

    Update minimum.js
    added use strict and changed out vars for lets.

--- minimum.js
+++ minimum.js
@@ -1,14 +1,16 @@
 // Write a function min that takes two argruements and returns their minimum.
 
 (function () {
-    var input1 = 6;
-    var input2 = 89;
-    
-    function min (a, b) {
+    "use strict";
+
+    let input1 = 6;
+    let input2 = 89;
+
+    function min(a, b) {
         return Math.min(a, b);
     }
-    
+
     console.log(`I will now return the minimum of the two numbers!
 
-The answer is ${min(input1, input2)}.`)
+The answer is ${min(input1, input2)}.`);
 }());
